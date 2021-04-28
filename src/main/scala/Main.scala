90b71c997e56116c9d958d013682ca696c61ead6
Adam Vandolder <adam.vandolder@gmail.com>
2021-04-28 16:34:36 -0400

    Move methods onto relevant types, use single mutable substition

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable
 import scala.util.parsing.combinator._
 
 @main def main() =
@@ -7,12 +8,12 @@ import scala.util.parsing.combinator._
     x z
   """
   val expr = Parser.parseAll(Parser.program, src) match
-    case Parser.Success(matched, _) => matched 
+    case Parser.Success(matched, _) => matched
     case Parser.Failure(msg, _) => throw Exception(s"Failure: $msg")
     case Parser.Error(msg, _) => throw Exception(s"Failure: $msg")
   println(expr)
-  val (s, t) = expr.typecheck(Map())
-  println(simplifyVars(normalize(s, t)))
+  val t = expr.typecheck
+  println(t.simplifyVars)
 
 enum Expr:
   case EBool(b: Boolean)
@@ -31,6 +32,10 @@ enum Expr:
     case EVar(v) => v
     case ELet(v, e1, e2) => s"let $v = $e1 in $e2"
     case EIf(e1, e2, e3) => s"if $e1 then $e2 else $e3"
+
+  def typecheck: Typ =
+    val infer = Infer()
+    infer.typecheck(this, Map())
 import Expr._
 
 enum Typ:
@@ -46,85 +51,78 @@ enum Typ:
     case TVar(n: Int) =>
       val primes = n / 26
       val char = (n % 26 + 'a').toChar
-      s"?$char${"'" * primes}" 
+      s"?$char${"'" * primes}"
+
+  def simplifyVars: Typ =
+    def simplify(t: Typ, m: Map[Int, Int]): Typ = t match
+      case TInt | TBool => t
+      case TVar(v) => TVar(m(v))
+      case TArr(t1, t2) => TArr(simplify(t1, m), simplify(t2, m))
+    val m = this.getVars.zipWithIndex.toMap
+    simplify(this, m)
+
+  def getVars: Set[Int] = this match
+    case TInt | TBool => Set()
+    case TVar(v) => Set(v)
+    case TArr(t1, t2) => t1.getVars ++ t2.getVars
+
+  def occurs(v: Int): Boolean = this match
+    case TInt | TBool => false
+    case TVar(v2) => v == v2
+    case TArr(t1, t2) => t1.occurs(v) || t2.occurs(v)
 import Typ._
 
-type Subst = Map[Int, Typ]
+final class Infer:
+  private val s: mutable.Map[Int, Typ] = mutable.Map()
+  private var varCount = 0
 
-def occurs(v: Int, t: Typ): Boolean = t match
-  case TInt | TBool => false
-  case TVar(v2) => v == v2
-  case TArr(t1, t2) => occurs(v, t1) || occurs(v, t2)
+  def freshVar(): Typ =
+    val v = varCount
+    varCount += 1
+    TVar(v)
 
-def getVars(t: Typ): Set[Int] = t match
-  case TInt | TBool => Set()
-  case TVar(v) => Set(v)
-  case TArr(t1, t2) => getVars(t1) ++ getVars(t2)
-
-def normalize(s: Subst, t: Typ): Typ = t match
-  case TInt | TBool => t
-  case TVar(v) => s.get(v).map(normalize(s, _)).getOrElse(t)
-  case TArr(t1, t2) => TArr(normalize(s, t1), normalize(s, t2))
-
-def simplifyVars(t: Typ): Typ =
-  def simplify(t: Typ, m: Map[Int, Int]): Typ = t match
+  def normalize(t: Typ): Typ = t match
     case TInt | TBool => t
-    case TVar(v) => TVar(m(v))
-    case TArr(t1, t2) => TArr(simplify(t1, m), simplify(t2, m))
-  val m = getVars(t).zipWithIndex.toMap
-  simplify(t, m)
+    case TVar(v) => s.get(v).map(normalize).getOrElse(t)
+    case TArr(t1, t2) => TArr(normalize(t1), normalize(t2))
 
-def merge(s1: Subst, s2: Subst): Subst =
-  val s = s1.keySet
-    .intersect(s2.keySet)
-    .foldLeft(Map())((s: Subst, v) => unify(s, s1(v), s2(v)))
-  s1 ++ s2 ++ s
+  def updateVar(v: Int, t: Typ) =
+    s.put(v, t).foreach(unify(_, t))
 
-def unify(s: Subst, t1: Typ, t2: Typ): Subst =
-  (normalize(s, t1), normalize(s, t2)) match
-    case (TInt, TInt) | (TBool, TBool) => s
-    case (TVar(v1), TVar(v2)) if v1 == v2 => s
-    case (TVar(v), t) if !occurs(v, t) => s + (v -> t)
-    case (t, TVar(v)) if !occurs(v, t) => s + (v -> t)
-    case (TArr(l1, r1), TArr(l2, r2)) =>
-      val s1 = unify(s, l1, l2)
-      val s2 = unify(s, r1, r2)
-      merge(s1, s2)
-    case (t1, t2) => throw Exception(s"unification failure: $t1 with $t2")
+  def unify(t1: Typ, t2: Typ): Unit =
+    (normalize(t1), normalize(t2)) match
+      case (TInt, TInt) | (TBool, TBool) => ()
+      case (TVar(v1), TVar(v2)) if v1 == v2 => ()
+      case (TVar(v), t) if !t.occurs(v) => updateVar(v, t)
+      case (t, TVar(v)) if !t.occurs(v) => updateVar(v, t)
+      case (TArr(l1, r1), TArr(l2, r2)) => unify(l1, l2); unify(r1, r2)
+      case (t1, t2) => throw Exception(s"unification failure: $t1 with $t2")
 
-var varCount = 0
-def freshVar(): Typ =
-  val v = varCount
-  varCount += 1
-  TVar(v)
-
-extension (e: Expr) def typecheck(ctx: Map[String, Typ]): (Subst, Typ) = e match
-  case EBool(_) => (Map(), TBool)
-  case EInt(_) => (Map(), TInt)
-  case EApp(e1, e2) =>
-    val (s1, t1) = e1.typecheck(ctx)
-    val (s2, t2) = e2.typecheck(ctx)
-    val t3 = freshVar()
-    val s = unify(merge(s1, s2), t1, TArr(t2, t3))
-    (s, normalize(s, t3))
-  case ELam(v, e) =>
-    val t1 = freshVar()
-    val (s, t2) = e.typecheck(ctx + (v -> t1))
-    (s, TArr(t1, normalize(s, t2)))
-  case EVar(v) => (Map(), ctx(v))
-  case ELet(v, e1, e2) =>
-    val (s1, t1) = e1.typecheck(ctx)
-    val (s2, t2) = e2.typecheck(ctx + (v -> t1))
-    val s = merge(s1, s2)
-    (s, normalize(s, t2))
-  case EIf(e1, e2, e3) =>
-    val (s1, t1) = e1.typecheck(ctx)
-    val (s2, t2) = e2.typecheck(ctx)
-    val (s3, t3) = e3.typecheck(ctx)
-    val s4 = unify(s1, t1, TBool)
-    val s5 = unify(merge(s2, s3), t2, t3)
-    val s = merge(s4, s5)
-    (s, normalize(s, t2))
+  def typecheck(e: Expr, ctx: Map[String, Typ]): Typ = e match
+    case EBool(_) => TBool
+    case EInt(_) => TInt
+    case EApp(e1, e2) =>
+      val t1 = typecheck(e1, ctx)
+      val t2 = typecheck(e2, ctx)
+      val t3 = freshVar()
+      unify(t1, TArr(t2, t3))
+      normalize(t3)
+    case ELam(v, e) =>
+      val t1 = freshVar()
+      val t2 = typecheck(e, ctx + (v -> t1))
+      TArr(t1, normalize(t2))
+    case EVar(v) => ctx(v)
+    case ELet(v, e1, e2) =>
+      val t1 = typecheck(e1, ctx)
+      val t2 = typecheck(e2, ctx + (v -> t1))
+      normalize(t2)
+    case EIf(e1, e2, e3) =>
+      val t1 = typecheck(e1, ctx)
+      val t2 = typecheck(e2, ctx)
+      val t3 = typecheck(e3, ctx)
+      unify(t1, TBool)
+      unify(t2, t3)
+      normalize(t2)
 
 object Parser extends RegexParsers, PackratParsers:
   override def skipWhitespace = true
